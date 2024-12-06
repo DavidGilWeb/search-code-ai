93f0330243948be67d74908ea71e1f3b11a3e968
DavidGilWeb <info@davidgilweb.com>
2024-12-06 15:15:36 +0100

    search files with selected code
    Open a project, select a code and write the next command: >Search Code

--- src/extension.ts
+++ src/extension.ts
@@ -1,42 +1,47 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { Console } from 'console';
 import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "search-code-ia" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('search-code-ia.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('search-code-ia.searchCode', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-
-        const editor = vscode.window.activeTextEditor;
+		let arrFiles = [];
+		const editor = vscode.window.activeTextEditor;
 
 		if (!editor) {
-            vscode.window.showErrorMessage('No hay ningún editor activo.');
-            return;
-        }
-
+			vscode.window.showErrorMessage('No hay ningún editor activo.');
+			return;
+		}
+	
 		const selection = editor.selection;
-        const selectedText = editor.document.getText(selection);
+		const selectedText = editor.document.getText(selection);
 
-		 // Muestra el texto seleccionado en un mensaje
-		 if (selectedText) {
-            vscode.window.showInformationMessage(`Texto seleccionado: ${selectedText}`);
-        } else {
-            vscode.window.showWarningMessage('No se seleccionó ningún texto.');
+		const allFiles = await vscode.workspace.findFiles('**/*.php');
+
+        for (const file of allFiles) {
+            const content = await vscode.workspace.openTextDocument(file);
+            const fileWithText = content.getText();
+
+            if (fileWithText.includes(selectedText)) {
+                arrFiles.push(file);
+            }
         }
+		if(arrFiles.length === 0){
+		vscode.window.showWarningMessage('No existe el texto seleccionado.');
+		}else{
+		vscode.window.showInformationMessage(`El texto seleccionado aparece en ${arrFiles.length} archivos`);
+		
+		}
 
-		//vscode.window.showInformationMessage('Hello World from search-code-ia!');
 	});
-
 	context.subscriptions.push(disposable);
 }
 
