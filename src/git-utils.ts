76551f6e061f8a0eaffdb884fa7520c82e82f9b7
Zafiro12 <alexsanta1205@gmail.com>
2025-01-18 12:38:03 +0100

    Added base Git utils class and types

--- src/git-utils.ts
+++ src/git-utils.ts
@@ -1,5 +1,65 @@
-class GitUtils{
-    constructor() {
-        
+import { extensions } from "vscode";
+import { GitExtension } from "./types/git";
+import { execute } from "./util/execute";
+import { dirname } from "node:path";
+
+class GitUtils {
+  filePath: string;
+
+  constructor(filePath: string) {
+    this.filePath = filePath;
+  }
+
+  /**
+   * Utility function to check if the file is in a git repository
+   * 
+   * @returns true if the file is in a git repository, false otherwise
+   */
+  async isGitRepository(): Promise<boolean> {
+    return this.getRepositoryRoot()
+      .then(() => true)
+      .catch(() => false);
+  }
+
+  /**
+   * Function to get the root of the git repository
+   * where the file is located
+   * 
+   * @returns root of the git repository
+   * @throws error if the file is not in a git repository
+   */
+  async getRepositoryRoot(): Promise<string> {
+    return this.runGitCommand(this.filePath, "rev-parse", "--show-toplevel");
+  }
+
+  /**
+   * Function to run a git command for a specific file
+   *
+   * @param filePath path to the file
+   * @param args arguments to pass to the git command
+   */
+  async runGitCommand(...args: string[]): Promise<string> {
+    return execute({
+      command: this.getGitCommand(),
+      args,
+      options: {
+        cwd: dirname(this.filePath),
+        env: { ...process.env, LC_ALL: "C" },
+      },
+    });
+  }
+
+  /**
+   * Function to get git command, be it
+   * from the vscode extension or the system
+   */
+  private getGitCommand(): string {
+    const vscodeGit = extensions.getExtension<GitExtension>("vscode.git");
+
+    if (vscodeGit?.exports.enabled) {
+      return vscodeGit.exports.getAPI(1).git.path;
     }
+
+    return "git";
+  }
 }
